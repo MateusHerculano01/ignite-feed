daa3e733ba00ca07a66fd5b3f7dfbb9946f469e2
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-08 14:43:31 -0300

    refactor: component avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,9 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/58828441?v=4" />
+        <Avatar src="https://avatars.githubusercontent.com/u/58828441?v=4" />
 
         <strong>Mateus Herculano</strong>
         <span>Web Developer</span>
